fd9fa3d09fcbcfef33caae83924f8d1d65fc0fce
Claus Lensbøl <cmol@cmol.dk>
2021-12-28 23:35:17 -0500

    Add rr build tests

--- resource_record_test.go
+++ resource_record_test.go
@@ -32,9 +32,7 @@ func TestParseRecord(t *testing.T) {
 				Class:       1,
 				TTL:         300,
 				RDataLength: 16,
-				Data: &IPv6{
-					IP: netaddr.MustParseIP("2607:f8b0:400b:802::2011"),
-				},
+				Data:        &IPv6{netaddr.MustParseIP("2607:f8b0:400b:802::2011")},
 			},
 		},
 		{
@@ -49,10 +47,16 @@ func TestParseRecord(t *testing.T) {
 				Class:       1,
 				TTL:         300,
 				RDataLength: 4,
-				Data: &IPv4{
-					IP: netaddr.MustParseIP("142.251.41.81"),
-				},
+				Data:        &IPv4{netaddr.MustParseIP("142.251.41.81")},
 			},
+		},
+		{
+			name: "Simple A record",
+			args: args{
+				buf:     []byte("\x06golang\x03com\x00\x00\x77\x00\x01\x00\x00\x01\x2c\x00\x04\x8e\xfb\x29\x51"),
+				domains: make(map[int]string),
+			},
+			wantErr: true,
 		},
 	}
 	for _, tt := range tests {
@@ -64,6 +68,72 @@ func TestParseRecord(t *testing.T) {
 			}
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("ParseRecord() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestRecord_Build(t *testing.T) {
+	type fields struct {
+		TTL         uint32
+		Class       uint16
+		RDataLength uint16
+		RType       Type
+		Name        string
+		Data        RData
+	}
+	type args struct {
+		domains map[string]int
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    []byte
+		wantErr bool
+	}{
+		{
+			name: "Build Simple AAAA record",
+			fields: fields{
+				TTL:         300,
+				Class:       1,
+				RDataLength: 16,
+				RType:       AAAA,
+				Name:        "golang.com",
+				Data:        &IPv6{netaddr.MustParseIP("2607:f8b0:400b:802::2011")},
+			},
+			want: []byte("\x06golang\x03com\x00\x00\x1c\x00\x01\x00\x00\x01\x2c\x00\x10\x26\x07\xf8\xb0\x40\x0b\x08\x02\x00\x00\x00\x00\x00\x00\x20\x11"),
+		},
+		{
+			name: "Build Simple A record",
+			fields: fields{
+				TTL:         300,
+				Class:       1,
+				RDataLength: 4,
+				RType:       A,
+				Name:        "golang.com",
+				Data:        &IPv4{netaddr.MustParseIP("142.251.41.81")},
+			},
+			want: []byte("\x06golang\x03com\x00\x00\x01\x00\x01\x00\x00\x01\x2c\x00\x04\x8e\xfb\x29\x51"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := &Record{
+				TTL:         tt.fields.TTL,
+				Class:       tt.fields.Class,
+				RDataLength: tt.fields.RDataLength,
+				RType:       tt.fields.RType,
+				Name:        tt.fields.Name,
+				Data:        tt.fields.Data,
+			}
+			buf := new(bytes.Buffer)
+			if err := r.Build(buf, tt.args.domains); (err != nil) != tt.wantErr {
+				t.Errorf("Record.Build() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(buf.Bytes(), tt.want) {
+				t.Errorf("Record.Build() = %v, want %v", buf.Bytes(), tt.want)
 			}
 		})
 	}
