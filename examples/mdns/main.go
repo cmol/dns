ba3626ea61db769aae40ed3df87a0f56b02330a6
Claus Lensbøl <cmol@cmol.dk>
2024-04-10 20:17:00 -0400

    Add simple MDNs example

---
+++ examples/mdns/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"net"
+	"os"
+
+	"github.com/cmol/dns"
+)
+
+const (
+	srvAddr = "224.0.0.251:5353"
+	// srvAddr     = "[ff02::fb]:5353"
+	maxDatagram = 1500
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Printf("Usage: %s [interface_name]\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	ifi, err := net.InterfaceByName(os.Args[1])
+	if err != nil {
+		fmt.Printf("Unable to find interface with name: %s \n", os.Args[1])
+		fmt.Printf("Usage: %s [interface_name]\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	addr, err := net.ResolveUDPAddr("udp", srvAddr)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	l, err := net.ListenMulticastUDP("udp", ifi, addr)
+	if err != nil {
+		panic(err.Error())
+	}
+	l.SetReadBuffer(maxDatagram)
+	for {
+		b := make([]byte, maxDatagram)
+		n, src, err := l.ReadFromUDP(b)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		buf := bytes.NewBuffer(b)
+		message, err := dns.ParseMessage(buf)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		fmt.Printf("Read %d bytes from %s%%%s containing: %+v\n", n, src.IP.String(), src.Zone, message)
+	}
+}
